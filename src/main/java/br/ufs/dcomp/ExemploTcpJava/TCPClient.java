2d8b3168dd1a061afd44eb87164e2b5bfcbbd05f
Ubuntu <ubuntu@ip-172-31-60-185.ec2.internal>
2024-01-18 20:26:14 +0000

    modificacoes em aula

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -5,22 +5,46 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class TCPClient{
     public static void main(String[] args){
         try {
-            System.out.print("[ Conectando com o Servidor TCP    ..................  ");
-            Socket sock = new Socket("127.0.0.1", 3300);
-            System.out.println("[OK] ]");
+            Socket sock = conectarComServidor("127.0.0.1",3300);
             
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
-            String msg = "Olá, DCOMP!!!";
-            byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+            byte[] buf_resposta = new byte[20]; // buffer de recepção
 
-            System.out.print("[ Enviando mensagem    ..............................  ");
-            os.write(buf);
-            System.out.println("[OK] ]");
+            while(true){
+                enviarMensagem(os);
+                receberMensagem(is,buf_resposta);
+            }
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
+    }
+    public static Socket conectarComServidor(String ip, Integer port) throws IOException {
+        System.out.print("[ Conectando com o Servidor TCP    ..................  ");
+        Socket sock = new Socket("127.0.0.1", 3300);
+        System.out.println("[OK] ]");
+        return sock;
+    }
+    private static void enviarMensagem(OutputStream os)throws IOException {
+        System.out.print("[ Enviando mensagem    ..............................  ");
+        String msgResposta = lerMensagem();
+        byte[] buf_resposta = msgResposta.getBytes(); // Obtendo a respresntação em bytes da mensagem
+        os.write(buf_resposta);
+    } 
+    private static void receberMensagem(InputStream is, byte[] buf)throws IOException {
+        System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
+        is.read(buf); // Operação bloqueante (aguardando chegada de dados)
+        System.out.println("[OK] ]");
+        String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+        System.out.println("  Mensagem recebida: "+ msg);
+    }
+    private static String lerMensagem (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a mensagem a ser enviada: ");
+        return scanner.nextLine();
     }
 }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -5,30 +5,54 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class TCPServer{
     public static void main(String[] args){
-        
+
         try {
-            System.out.print("[ Iniciando Servidor TCP    .........................  ");
-            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
-            System.out.println("[OK] ]");
-            
-            System.out.print("[ Aquardando pedidos de conexão    ..................  ");
-            Socket sock = ss.accept(); // Operação bloqueante (aguardando pedido de conexão)
-            System.out.println("[OK] ]");
+            Socket sock = iniciarServidor(3300, 5, "127.0.0.1");
             
             InputStream is = sock.getInputStream(); //Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); //Canal de saída de dados
             byte[] buf = new byte[20]; // buffer de recepção
-
-            System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
-            is.read(buf); // Operação bloqueante (aguardando chegada de dados)
-            System.out.println("[OK] ]");
             
-            String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+            while (true){
+                receberMensagem(is,buf);
+                enviarMensagem(os);
+            }
             
-            System.out.println("  Mensagem recebida: "+ msg);
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
+    private static Socket iniciarServidor(int port, int backlog, String ipAddress) throws IOException {
+        System.out.print("[ Iniciando Servidor TCP    .........................  ");
+        ServerSocket ss = new ServerSocket(port, backlog, InetAddress.getByName(ipAddress));
+        System.out.println("[OK] ]");
+        System.out.print("[ Aguardando pedidos de conexão    ..................  ");
+        Socket sock = ss.accept(); // Operação bloqueante (aguardando pedido de conexão)
+        System.out.println("[OK] ]");
+        return sock;
+    }
+    
+    private static void receberMensagem(InputStream is, byte[] buf)throws IOException {
+        System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
+        is.read(buf); // Operação bloqueante (aguardando chegada de dados)
+        System.out.println("[OK] ]");
+        String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+        System.out.println("  Mensagem recebida: "+ msg);
+    }
+    
+    private static void enviarMensagem(OutputStream os)throws IOException {
+        System.out.print("[ Enviando mensagem    ..............................  ");
+        String msgResposta = lerMensagem();
+        byte[] buf_resposta = msgResposta.getBytes(); // Obtendo a respresntação em bytes da mensagem
+        os.write(buf_resposta);
+    } 
+    private static String lerMensagem (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a mensagem a ser enviada: ");
+        return scanner.nextLine();
+    }
+
 }
